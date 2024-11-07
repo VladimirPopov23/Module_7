c2f5d6afe95b62d5c3d3f6e3d735e939c7207f8d
VladimirPopov23 <volodyaklever@gmail.com>
2024-11-07 20:40:23 +0300

    module_7_2.py
    07.11.2024 Задача "Записать и запомнить"

--- main.py
+++ main.py
@@ -1,52 +1,30 @@
-# module_7_1.py
-# 07.11.2024 в2. Задача "Учёт товаров"
-
+# module_7_2.py
+# 07.11.2024 Задача "Записать и запомнить"
 from pprint import pprint
 
-
-class Product:
-    def __init__(self, name: str, weight: float, category: str):
-        self.name: str = name  # название продукта (строка)
-        self.weight: float = weight  # общий вес товара (дробное число)
-        self.category: str = category  # категория товара (строка)
-
-    def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}'
-
-
-class Shop:
-    __file_name = 'products.txt'
-    file = open(__file_name, 'r')  # создаем новый файл
-    file.close()
-
-    def get_products(self):
-        file = open(self.__file_name, 'r')
-        products = file.read()
-        file.close()
-        return products
-
-    def add(self, *products):
-        products_if_file = self.get_products()
-        file = open(self.__file_name, 'a')
-        for product in products:
-            if products_if_file.find(f'{product.name}') == -1:  # find возвращает индекс символа, с которого начинается
-                                                                # искомая строка, в случае неудачи возвращает -1
-                file = open(self.__file_name, 'a')
-                file.write(f'{product}\n')
-            else:
-                print(f'Продукт {product} уже есть в магазине')
-        file.close()
+def custom_write(file_name, strings):
+    file_name = 'file_name.txt' # имя файла
+    file = open(file_name, 'w', encoding = 'utf-8') # открываем файл с кодировкой 'utf-8'
+    strings_positions = {} # словарь
+    str_num = 0 # номер строки
+    byte_start_str = file.seek(0) # байт начала первой строки
+    for string in strings:
+        file.write(f'{string}\n') # записываем строку из списка
+        str_num += 1 # следующая строка
+        key = (str_num, byte_start_str) # ключ словаря
+        strings_positions[key]= string # значения словаря - записываемая строка
+        byte_start_str = file.tell() # байт начала следующей строки
+    file.close() # закрываем файл
+    return strings_positions # возвращаем получившийся словарь
 
 
-if __name__ == "__main__":
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
 
-    s1 = Shop()
-    p1 = Product('Potato', 50.5, 'Vegetables')
-    p2 = Product('Spaghetti', 3.4, 'Groceries')
-    p3 = Product('Potato', 5.5, 'Vegetables')
-
-    print(p2) # __str__
-
-    s1.add(p1, p2, p3)
-
-    print(s1.get_products())
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
