8b00d088ea2acbbd44a71e97e65bbebdf1fb5c82
VladimirPopov23 <volodyaklever@gmail.com>
2024-11-12 18:24:36 +0300

    module_7_3.py
    12.11.2024 Задача "Найдёт везде"

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-# module_7_2.py
-# 07.11.2024 Задача "Записать и запомнить"
-from pprint import pprint
+# module_7_3.py
+# 12.11.2024 Задача "Найдёт везде"
 
-def custom_write(file_name, strings):
-    file_name = 'file_name.txt' # имя файла
-    file = open(file_name, 'w', encoding = 'utf-8') # открываем файл с кодировкой 'utf-8'
-    strings_positions = {} # словарь
-    str_num = 0 # номер строки
-    byte_start_str = file.seek(0) # байт начала первой строки
-    for string in strings:
-        file.write(f'{string}\n') # записываем строку из списка
-        str_num += 1 # следующая строка
-        key = (str_num, byte_start_str) # ключ словаря
-        strings_positions[key]= string # значения словаря - записываемая строка
-        byte_start_str = file.tell() # байт начала следующей строки
-    file.close() # закрываем файл
-    return strings_positions # возвращаем получившийся словарь
+class WordsFinder:
+
+    def __init__(self, *file_names):
+        self.file_names = file_names  # атрибут принимающий неограниченное кол-во названий файлов
+        # file_names = name.append()
+
+    def get_all_words(self):
+        all_words = {}  # словарь для записи данных, (ключ - название файла, значение - список из слов этого файла)
+        for name in self.file_names:  # цикл для того что бы перебрать названия файлов
+            with open(name, 'r', encoding='utf-8') as file:  # оператор with поочередно открывает файлы в utf-8
+                info = file.read().lower()  # .read() считывает файл в строку и .lower() переводит элементы в н.регистр
+                for s in [',', '.', '=', '!', '?', ';', ':', ' - ']:  # цикл для поиска пунктуации в s строках файла
+                    info = info.replace(s, '')  # .replace() заменяет знаки пунктуации на ничто ''
+                all_words[name] = info.split()  # переписывает значение в файле name без пунктуации, но разделив .split() строку
+        return all_words
+
+    # find(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла, значение - позиция первого такого слова в списке слов этого файла.
+    def find(self, word):
+        find_word = {}  # словарь, где ключ - название файла, значение - позиция первого такого слова в файле
+        for name, words in self.get_all_words().items():  # цикл для поиска слов в списке слов этого файла по ключу
+            if word.lower() in words:  # если слово (в н.регисте) есть в значении списка all_words (метод get_all_words)
+                find_word[name] = words.index(word.lower()) + 1  # то по ключу name присваеваем words значение позиции в последовательности, c учетом, что первый эл-т - 0
+                find_word.update()  # добавляем ключ/значение в словарь
+        return find_word  # по завершению цикла возвращаем словарь find_word
+
+    # count(self, word) - метод, где word - искомое слово. Возвращает словарь, где ключ - название файла, значение - количество слова word в списке слов этого файла.
+    def count(self, word):
+        count_word = {}  # словарь, где ключ - название файла, значение - количество слов 'word' в файле
+        for name, words in self.get_all_words().items():  # цикл для поиска слов в списке слов этого файла по ключу
+            count_word[name] = words.count(word.lower())  # .count считает кол-во слов (в н.регисте) в списке слов файла name
+        return count_word  # по завершению цикла возвращаем словарь count_word
 
 
-info = [
-    'Text for tell.',
-    'Используйте кодировку utf-8.',
-    'Because there are 2 languages!',
-    'Спасибо!'
-    ]
 
-result = custom_write('test.txt', info)
-for elem in result.items():
-  print(elem)
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
